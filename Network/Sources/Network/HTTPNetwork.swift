68d5de1157964c3f44a72513726b7ddc5991fc90
iOSCS <dbrckstla@naver.com>
2021-12-26 21:11:44 +0900

    Network Package의 디렉토리 수정(NetwrokRequestable 추가)

--- Network/Sources/Network/HTTPNetwork.swift
+++ Network/Sources/Network/HTTPNetwork.swift
@@ -5,40 +5,7 @@
 //  Created by 육찬심 on 2021/12/26.
 //
 
-import Foundation
 import Alamofire
-
-public enum NetworkError: Error {
-    
-    case invalidURL
-}
-
-public protocol RequestTarget: URLConvertible {
-    
-    var baseURLString: String { get }
-    var path: String { get }
-    var headers: Alamofire.HTTPHeaders { get }
-    var bodyPrameters: Alamofire.Parameters? { get }
-    var method: Alamofire.HTTPMethod { get }
-    var parameterEncoding: Alamofire.ParameterEncoding { get }
-    
-    var timeoutInterval: TimeInterval { get }
-}
-
-public extension RequestTarget {
-    
-    func asURL() throws -> URL {
-        guard let url = URL(string: baseURLString)?.appendingPathComponent(path) else {
-            throw NetworkError.invalidURL
-        }
-        return url
-    }
-}
-
-public protocol NetworkRequestable {
-    
-    func request<T: Decodable>(target: RequestTarget) async -> Result<T, Error>
-}
 
 public struct HTTPNetwork: NetworkRequestable {
     
