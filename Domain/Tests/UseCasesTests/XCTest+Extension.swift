4e3eaac55857516488cc599090699fd3d32defb1
iOSCS <dbrckstla@naver.com>
2021-12-26 18:32:20 +0900

    async / await 테스트 용이하도록 asyncTest 블럭 추가

---
+++ Domain/Tests/UseCasesTests/XCTest+Extension.swift
@@ -0,0 +1,21 @@
+//
+//  XCTest+Extension.swift
+//  
+//
+//  Created by 육찬심 on 2021/12/26.
+//
+
+import XCTest
+
+extension XCTestCase {
+    
+    func asyncTest(timeout: TimeInterval = 1, asyncBlock: @escaping () async -> Void) {
+        let expectation = expectation(description: #function)
+        Task {
+            await asyncBlock()
+            expectation.fulfill()
+        }
+        
+        waitForExpectations(timeout: timeout)
+    }
+}
